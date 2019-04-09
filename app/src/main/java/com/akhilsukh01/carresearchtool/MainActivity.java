948b050ff488f81e991a21345c2202def91df15f
akhil <akhilsukh01@gmail.com>
2019-04-09 08:45:45 -0700

    day 1: added budget and manufacturer spinners

--- app/src/main/java/com/akhilsukh01/carresearchtool/MainActivity.java
+++ app/src/main/java/com/akhilsukh01/carresearchtool/MainActivity.java
@@ -2,6 +2,8 @@ package com.akhilsukh01.carresearchtool;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner spinner_budgets = (Spinner) findViewById(R.id.spinner_budgets);
+        Spinner spinner_manu = (Spinner) findViewById(R.id.spinner_manu);
+
+        ArrayAdapter<CharSequence> adapter_budgets = ArrayAdapter.createFromResource(this, R.array.budgets, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> adapter_manu = ArrayAdapter.createFromResource(this, R.array.manufacturers, android.R.layout.simple_spinner_item);
+
+        adapter_budgets.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapter_manu.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner_budgets.setAdapter(adapter_budgets);
+        spinner_manu.setAdapter(adapter_manu);
+
     }
 }
